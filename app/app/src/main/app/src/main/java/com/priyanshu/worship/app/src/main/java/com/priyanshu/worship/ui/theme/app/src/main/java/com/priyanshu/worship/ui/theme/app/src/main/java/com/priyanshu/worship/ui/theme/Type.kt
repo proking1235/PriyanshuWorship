26a10da23513575bc91807024e3a734c4eaf3c0d
proking1235 <modipriyanshu673@gmail.com>
2025-07-26 11:25:19 +0530

    Create Type.kt

---
+++ app/app/src/main/app/src/main/java/com/priyanshu/worship/app/src/main/java/com/priyanshu/worship/ui/theme/app/src/main/java/com/priyanshu/worship/ui/theme/app/src/main/java/com/priyanshu/worship/ui/theme/Type.kt
@@ -0,0 +1,22 @@
+package com.priyanshu.worship.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+
+// Typography setup
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 16.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 24.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 12.sp
+    )
+)
